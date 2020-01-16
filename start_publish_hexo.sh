d19c09ef17562f555c4ff0c05c66c80ddeefc239
chenren <chenren1@staff.sina.com.cn>
2020-01-16 23:55:10 +0800

    脚本修改

--- start_publish_hexo.sh
+++ start_publish_hexo.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # 1、发布博客到线上。2上传项目数据到git
 
-# 获取git commit 参数
-# if [ ! -n "$1" ]; 
-# then
-#   echo "commit des is null"
-#   exit 0
-# fi
-# commit_msg=$1
-# echo "第一个参数：" $1
+# 获取 commit 改动信息
+read -p "请输入改动信息:" commit_msg
 
-commit_msg="auto commit"
+if [ ! -n "$commit_msg" ]; 
+then
+  echo "commit des is null, auto commit"
+  commit_msg="auto commit"
+fi
+
+echo "提交改动：" $commit_msg
 
 hexo clean
 git status
--- test_print.sh
+++ test_print.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
+
+
+read -p "请输入改动信息:" commit_msg
+
+
+if [ ! -n "$commit_msg" ]; 
+then
+  echo "commit des is null, auto commit"
+  commit_msg="auto commit"
+fi
+
 echo "参数：" $1
+
+echo "改动信息：" $commit_msg
